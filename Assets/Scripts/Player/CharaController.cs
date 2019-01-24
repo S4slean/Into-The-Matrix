0c26865b5aceae82f1998cc023ba573e0eaffbc1
Stanislas <s.dubuc@rubika-edu.com>
2019-01-24 16:39:04 +0100

    Controller Movement

--- Assets/Scripts/Player/CharaController.cs
+++ Assets/Scripts/Player/CharaController.cs
@@ -5,45 +5,70 @@ using UnityEngine;
 
 public class CharaController : MonoBehaviour
 {
-	public enum Direction{ None, Right, Left, Up, Down	}
+	public float stepDistance;
+	public int moveStep = 30;
+	public float stepDuration = 1;
 
-	public Direction dir;
+	[SerializeField] bool ismoving = false;
 	private Vector3 startMousePos;
+	private Vector3 hitPosition;
 
-	private void Update()
+
+	private void OnMouseDown()
 	{
-		GetInput();
-		MoveTo();
+		startMousePos = transform.position;
+		print(startMousePos);
+	}
+
+	private void OnMouseUp()
+	{
+		if (Input.GetMouseButtonUp(0) && !ismoving)
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit)) //check if the ray hit something
+			{
+				hitPosition = hit.point; //use this position for what you want to do
+			}
+			else
+				hitPosition = startMousePos;
+
+			print(hitPosition);
+
+			Vector3 swipe = hitPosition - startMousePos;
+
+			if (swipe.magnitude < 1)
+				return;
+
+			if (Mathf.Abs(swipe.x) > Mathf.Abs(swipe.z))
+			{
+				int step = Mathf.RoundToInt(swipe.x / stepDistance);
+				ismoving = true;
+				StartCoroutine(Move(Vector3.right * Mathf.Sign(step), step));
+			}
+
+			if (Mathf.Abs(swipe.x) < Math.Abs(swipe.z))
+			{
+				int step = Mathf.RoundToInt(swipe.z / stepDistance);
+				ismoving = true;
+				StartCoroutine(Move(Vector3.forward * Mathf.Sign(step), step));
+			}
+		}
 	}
 
 
 
-	private void GetInput()
+	IEnumerator Move(Vector3 axe, int step)
 	{
-		if (Input.GetMouseButtonDown(0))
+		Debug.Log(step);
+
+		for (int i = 0; i < Mathf.Abs(moveStep * step); i++)
 		{
-			startMousePos = Input.mousePosition;
+			transform.localPosition = transform.localPosition + (axe / moveStep) * 2;
+
+			yield return new WaitForSeconds(stepDuration/moveStep);
 		}
-
-
-		if (Input.GetMouseButtonUp(0))
-		{
-			Vector3 swipe = Input.mousePosition - startMousePos;
-			dir = Direction.None;
-			if (swipe.x > 10 && Mathf.Abs(swipe.y) < 10)
-				dir = Direction.Right;
-			if (swipe.x < 10 && Mathf.Abs(swipe.y) < 10)
-				dir = Direction.Left;
-			if (swipe.y > 10 && Mathf.Abs(swipe.x) < 10)
-				dir = Direction.Up;
-			if (swipe.y < 10 && Mathf.Abs(swipe.x) < 10)
-				dir = Direction.Down;
-		}
-
-	}
-
-	private void MoveTo()
-	{
-		
+		ismoving = false;
 	}
 }
