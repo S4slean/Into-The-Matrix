d7885e77134f28b6a79f1baf14bdc20c5748297b
Stanislas <s.dubuc@rubika-edu.com>
2019-01-24 17:02:40 +0100

    Controller Move Rework

--- Assets/Scripts/Player/CharaController.cs
+++ Assets/Scripts/Player/CharaController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class CharaController : MonoBehaviour
 {
+	[Range (0, 300) ]public int swipeTolerance = 30;
 	public float stepDistance;
 	public int moveStep = 30;
 	public float stepDuration = 1;
@@ -14,48 +15,46 @@ public class CharaController : MonoBehaviour
 	private Vector3 hitPosition;
 
 
-	private void OnMouseDown()
-	{
-		startMousePos = transform.position;
-		print(startMousePos);
-	}
 
-	private void OnMouseUp()
+	private void Update()
 	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			startMousePos = Input.mousePosition;
+			print(startMousePos);
+		}
+
+
 		if (Input.GetMouseButtonUp(0) && !ismoving)
 		{
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-			if (Physics.Raycast(ray, out hit)) //check if the ray hit something
-			{
-				hitPosition = hit.point; //use this position for what you want to do
-			}
-			else
-				hitPosition = startMousePos;
+			hitPosition = Input.mousePosition;
 
 			print(hitPosition);
 
 			Vector3 swipe = hitPosition - startMousePos;
 
-			if (swipe.magnitude < 1)
+			if (swipe.magnitude < swipeTolerance)
 				return;
 
-			if (Mathf.Abs(swipe.x) > Mathf.Abs(swipe.z))
+			if (Mathf.Abs(swipe.x) > Mathf.Abs(swipe.y))
 			{
+				print("yo");
 				int step = Mathf.RoundToInt(swipe.x / stepDistance);
 				ismoving = true;
-				StartCoroutine(Move(Vector3.right * Mathf.Sign(step), step));
+				StartCoroutine(Move(Vector3.right * Mathf.Sign(step), 1));
 			}
 
-			if (Mathf.Abs(swipe.x) < Math.Abs(swipe.z))
+			if (Mathf.Abs(swipe.x) < Math.Abs(swipe.y))
 			{
-				int step = Mathf.RoundToInt(swipe.z / stepDistance);
+				int step = Mathf.RoundToInt(swipe.y / stepDistance);
 				ismoving = true;
-				StartCoroutine(Move(Vector3.forward * Mathf.Sign(step), step));
+				StartCoroutine(Move(Vector3.forward * Mathf.Sign(step), 1));
 			}
 		}
 	}
+
+	
 
 
 
