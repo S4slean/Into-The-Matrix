d434c5e5a9c9e198a66335b72d571add4eaf7f29
Stanislas <s.dubuc@rubika-edu.com>
2019-01-31 18:36:10 +0100

    Camera System

---
+++ Assets/Scripts/LevelDesign/RoomCameraTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+
+public class RoomCameraTrigger : MonoBehaviour
+{
+
+	public GameObject virtualCam;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			if(Camera.main.GetComponent<CinemachineBrain>().ActiveVirtualCamera != null)
+				Camera.main.GetComponent<CinemachineBrain>().ActiveVirtualCamera.VirtualCameraGameObject.SetActive(false);
+			virtualCam.SetActive(true);
+		}
+	}
+}
