0e4ee91a5485428fa1846289c9f0825124aea9e8
Antoine Fornies <pandaman110398@gmail.com>
2019-02-06 15:52:11 +0100

    Update HUd/ Debut Porte

---
+++ Assets/Scripts/GameManager/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+
+    [Header("Test")]
+
+    public GameObject SorcierPanel;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActiveSorcierPanel()
+    {
+        SorcierPanel.SetActive(true);
+    }
+
+    public void Menu ()
+    {
+        SorcierPanel.SetActive(false);
+    }
+}
