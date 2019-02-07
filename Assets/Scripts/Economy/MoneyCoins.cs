34a7a04ab041188e7b25266679a4f5b6652196ad
Unknown <nils.vandamme98@gmail.com>
2019-02-07 11:42:16 +0100

    Economy system base
    Created basic economy system:
    - Player money manager
    - Coins pick up

---
+++ Assets/Scripts/Economy/MoneyCoins.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyCoins : MonoBehaviour
+{
+    public int value; // Valeur de la pièce
+    public PlayerMoneyManager PMM; // Le PlayerMoneyManager
+
+    void Start()
+    {
+        PMM = GameObject.Find("Player").GetComponent<PlayerMoneyManager>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("Coin picked up !");
+            PMM.AddMoney(value);
+            Destroy(gameObject);
+        }
+    }
+}
