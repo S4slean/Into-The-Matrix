07aef57108417f06450f0cd3c89c03e3fca5174c
Unknown <nils.vandamme98@gmail.com>
2019-02-11 11:32:21 +0100

    HUB - Merchants
    Scene Antoine: Built a hub for the player and added Merchants NPCs

---
+++ Assets/Scripts/PNJs/PNJ_Merchant.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PNJ_Merchant : MonoBehaviour
+{
+    public GameObject MerchantInterface; // Interface du marchand sur l'UI, qui doit s'activer quand le joueur interragit avec le PNJ
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "AttackCollider")
+        {
+            MerchantInterface.SetActive(true);
+        }
+    }
+}
