0f47dbb255c683884622cf4d490ff78a09bcee3e
NDeev <awned@hotmail.fr>
2019-03-05 17:06:49 +0100

    Trap class à faire

---
+++ Assets/Scripts/LevelDesign/TrapClass.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapClass : MonoBehaviour
+{
+    public bool on;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    void Activate()
+    {
+        on = true;
+    }
+
+    void Desactivate()
+    {
+        on = false;
+    }
+}
