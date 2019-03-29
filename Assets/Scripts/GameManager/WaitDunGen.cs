f6acc85b620a2255c566e9af84ee219191d28596
Stanislas <s.dubuc@rubika-edu.com>
2019-03-29 17:05:13 +0100

    rename fix

--- Assets/Scripts/GameManager/WaitDunGen.cs
+++ Assets/Scripts/GameManager/WaitDunGen.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaitForDungeonGeneration : MonoBehaviour
+public class WaitDunGen: MonoBehaviour
 {
 	TempsPlongee diveTime;
 	GameObject player;
