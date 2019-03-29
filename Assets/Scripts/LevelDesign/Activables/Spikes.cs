1962fe94994600a64b7961a84312e3edcf339009
Stanislas <s.dubuc@rubika-edu.com>
2019-03-29 14:51:58 +0100

    Spikes

--- Assets/Scripts/LevelDesign/Activables/Spikes.cs
+++ Assets/Scripts/LevelDesign/Activables/Spikes.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Spikes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	Animator anim;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public bool isActive = true;
+	public bool cycling = true;
+	public float activeTime = 3;
+	public float inactiveTime = 3;
+
+	[SerializeField] float count;
+
+	private void Start()
+	{ 
+		anim = GetComponent<Animator>();
+	}
+
+	private void Update()
+	{
+		if (!cycling)
+			return;
+
+		if (isActive && count > activeTime || !isActive && count > inactiveTime)
+			Activate();
+
+		count += Time.deltaTime;
+	}
+
+	public void Activate()
+	{
+		isActive = !isActive;	
+		anim.SetBool("isActive", isActive);
+		count = 0;
+	}
+
 }
