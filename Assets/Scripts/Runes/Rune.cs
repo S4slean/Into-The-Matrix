539c577dc50839e31063214f0d29533088d6e95f
Stanislas <s.dubuc@rubika-edu.com>
2019-04-01 11:17:27 +0200

    meta and rune framework

---
+++ Assets/Scripts/Runes/Rune.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rune : MonoBehaviour
+{
+	public string name;
+	public string description;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
