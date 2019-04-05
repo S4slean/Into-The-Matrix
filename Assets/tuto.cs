bd4f7edef0dd16906a715320da7672ba0def18ba
Stanislas <s.dubuc@rubika-edu.com>
2019-04-05 09:46:08 +0200

    tuto WIP

---
+++ Assets/tuto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tuto : MonoBehaviour
+{
+	public GameObject tutoSwipe;
+	public GameObject tutoInteract;
+	public GameObject tutoSkill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		tutoSwipe.SetActive(true);
+    }
+
+}
