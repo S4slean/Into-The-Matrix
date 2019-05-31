ceb9c6dc87ea4e4884b5607cb8113b834f1b6714
NDeev <awned@hotmail.fr>
2019-05-31 18:27:23 +0200

    TimeBar improved, timeBonus created

--- Assets/Scripts/GameManager/TempsPlongee.cs
+++ Assets/Scripts/GameManager/TempsPlongee.cs
@@ -9,23 +9,71 @@ public class TempsPlongee : MonoBehaviour
     public float timeMax;
     public float timer;
     public Image timeBar;
+    public bool timeChange;
+    public PlayerStats statsScript;
 
     void Start()
     {
 		timeBar = GetComponent<Image>();
         timer = timeMax;
+        statsScript = FindObjectOfType<PlayerStats>();
     }
     
     void Update()
     {
         if (plongee)
         {
-            if (timer >= 0)
+            if (timer > 0)
             {
                 timer -= Time.deltaTime;
             }
         }
         timeBar.fillAmount = timer / timeMax;
-        timeBar.color = Color.Lerp(Color.red, Color.white, timer / timeMax);
+        if (!timeChange)
+        { timeBar.color = Color.Lerp(Color.red, Color.white, timer / timeMax); }
+        if (timer > timeMax)
+        { timer = timeMax; }
+
+        //commandes de Test
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        { StartCoroutine(TimeGain(5)); }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        { StartCoroutine(TimeLoss(5)); }
+
+        if (timer <= 0)
+        {
+            Debug.Log("Plus de temps");
+            statsScript.BackToLobby();
+        }
+    }
+
+    public IEnumerator TimeLoss(int timeLost)
+    {
+        timer -= timeLost;
+        timeChange = true;
+        timeBar.color = Color.red;
+        yield return new WaitForSecondsRealtime(0.05f);
+        timeBar.color = Color.white;
+        yield return new WaitForSecondsRealtime(0.05f);
+        timeBar.color = Color.red;
+        yield return new WaitForSecondsRealtime(0.05f);
+        timeBar.color = Color.white;
+        yield return new WaitForSecondsRealtime(0.05f);
+        timeChange = false;
+    }
+
+    public IEnumerator TimeGain(int timeGained)
+    {
+        timer += timeGained;
+        timeChange = true;
+        timeBar.color = Color.green;
+        yield return new WaitForSecondsRealtime(0.05f);
+        timeBar.color = Color.white;
+        yield return new WaitForSecondsRealtime(0.05f);
+        timeBar.color = Color.green;
+        yield return new WaitForSecondsRealtime(0.05f);
+        timeBar.color = Color.white;
+        yield return new WaitForSecondsRealtime(0.05f);
+        timeChange = false;
     }
 }
