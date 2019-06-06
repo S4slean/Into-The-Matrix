4ce42438b37de50f8588ad1d321490b66a3ea5d6
antoine fornies <pandaman110398@gmail.com>
2019-06-06 23:49:57 +0200

    Correctif Texture Wall et Integration Systéme audio
    Audio test

---
+++ Assets/Scripts/GameManager/Audio/AudioManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f,1f)]
+    public float volume=0.3f;
+    [Range(0.5f, 1.5f)]
+    public float pitch= 1f;
+
+    private AudioSource source;
+    public void setsource (AudioSource _source)
+    {
+        source = _source;
+        source.clip = clip;
+    }
+
+    public void Play ()
+    {
+        source.volume = volume;
+        source.pitch = pitch;
+        source.Play();
+    }
+}
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    [SerializeField]
+    Sound[] sounds;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("Plusieur audioManger dans la scéne");
+        }
+        else
+        {
+            instance = this;
+        }
+        
+    }
+    private void Start()
+    {
+        for (int i = 0; i < sounds.Length;i++)
+        {
+            GameObject _go = new GameObject("Sound_" + i+"_" + sounds[i].name);
+            _go.transform.SetParent(this.transform);
+            sounds[i].setsource(_go.AddComponent<AudioSource>());
+        }
+
+        
+    }
+
+    public void Playsound (string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].name == _name)
+            {
+                sounds[i].Play();
+                return;
+            }
+        }
+
+        // Il ne trouve pas le son
+
+        Debug.LogWarning("AudioManager : Sound not found in list :" + _name);
+    }
+}
---
+++ Assets/Scripts/GameManager/Audio/testson.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testson : MonoBehaviour
+{
+    public AudioSource Bonus;
+    public AudioSource Deathrobot;
+    public AudioSource GetRuneSound;
+    public AudioSource TP;
+    public AudioSource lasserShot;
+    public AudioSource LevierOn;
+    public AudioSource Reflect;
+    public AudioSource PhoneRing;
+    public AudioSource StartShield;
+
+    private void Start()
+    {
+      
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("a");
+            Bonus.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Deathrobot.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GetRuneSound.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            TP.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            lasserShot.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            LevierOn.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Reflect.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            PhoneRing.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            StartShield.Play();
+        }
+    }
+}
