f323363ba55fefd3dd3cb9abf6a403ff3e106b41
Stanislas <s.dubuc@rubika-edu.com>
2019-06-13 17:12:52 +0200

    FeedBacks

--- Assets/Scripts/Skills/SkillBar.cs
+++ Assets/Scripts/Skills/SkillBar.cs
@@ -91,6 +91,6 @@ public class SkillBar : MonoBehaviour
         skillsShop = FindObjectOfType<PNJ_DoorKeeper_MySkills>();
         //buttonsShop = GameObject.Find("HubUIv2").transform.GetChild(0).GetChild(4).GetChild(0).GetChild(0).GetComponent<PNJ_Merchant_InstantiateButtons>();
         skillsShop.UpdateMySkillNumber();
-        buttonsShop.ResetShopButtons();
+        //buttonsShop.ResetShopButtons();
     }
 }
